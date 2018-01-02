4f7fb14e35d2f817e860150e4908662216758226
geebeezzzz <30723719+geebeezzzz@users.noreply.github.com>
2018-01-02 15:21:30 +0100

    ugh01

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,7 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('lottie'), // Required
-  path: 'data.json', // Required
   renderer: 'svg', // Required
   loop: true, // Optional
   autoplay: true, // Optional
-  name: "Hello World", // Name for future reference. Optional.
+  path: 'data.json', // Required
 })
