79419394370ae31ef8dc73b8dfd2755faddd6e32
geebeezzzz <30723719+geebeezzzz@users.noreply.github.com>
2018-01-02 15:06:37 +0100

    update

--- Scripts/index.js
+++ Scripts/index.js
@@ -1,7 +1,7 @@
 var animation = bodymovin.loadAnimation({
   container: document.getElementById('lottie'), // Required
   path: 'data.json', // Required
-  renderer: 'svg/canvas/html', // Required
+  renderer: 'svg', // Required
   loop: true, // Optional
   autoplay: true, // Optional
   name: "Hello World", // Name for future reference. Optional.
