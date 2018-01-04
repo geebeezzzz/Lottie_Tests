0c02f7d741cd27619ab0ac01ca80da1b52354fe6
geebeezzzz <30723719+geebeezzzz@users.noreply.github.com>
2018-01-04 14:47:44 +0100

    Preview01

--- scripts/index.js
+++
@@ -1,7 +0,0 @@
-var animation = bodymovin.loadAnimation({
-  container: document.getElementById('lottie'), // Required
-  renderer: 'svg', // Required
-  loop: true, // Optional
-  autoplay: true, // Optional
-  path: 'data.json', // Required
-})
